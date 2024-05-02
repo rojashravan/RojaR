91a544523fb80f0aa9e099d38d8df006a2a2e253
roja <deekondaroja11@gmail.com>
2024-05-02 10:18:01 +0530

    operators

---
+++ Operators/index.js
@@ -0,0 +1,74 @@
+
+
+//1)Arithmatic operators
+// var x=10
+// var y=20
+// console.log(x+y)//Add
+// console.log(x-y)//sub
+// console.log(x*y)//mul
+// console.log(x/y)//division
+// console.log(x%y)//reminders of division
+// console.log(x**y)//exponentional
+// console.log(++x)//pre-incremenal 11
+// s//post-incremental 11
+// console.log(x) //12
+// console.log(--x)//pre-decremental 11
+// console.log(x--)//post-decremental 11
+// console.log(x)//10
+//2)Assignment operator
+// var x=10
+// console.log(x+=5)
+// console.log(x-=5)
+// console.log(x*=5)
+// console.log(x%=5)
+// console.log(x/=5)
+// console.log(x**=5)
+
+
+//3)comparision operators
+//1)relational operators
+// console.log(x>y)
+// console.log(x>=y)
+// console.log(x<y)
+// console.log(x<=y)
+// var x=10
+// var y=11
+// var y='10'
+// var x=undefined
+// var y=null
+// var x={value:10}
+// var y={value:10}
+// var x={value:10}
+// var y=x
+// var x=[]
+// var y=[]
+var x=[1,2,3]
+var y=x
+//2)Equality operators
+// console.log(x==y)//loose equality(it compares only value)
+// console.log(x===y)//strict equality(it compares value and data type)
+//Inequality
+// console.log(x!=y)//loose inequality(it compares value)
+// console.log(x!==y)//strict inequality(it compares value and datatype)
+
+//4)Ternary
+// var age=15
+// var age=20
+//var vote =age>=18?'having voterid':'not having voterid'
+//console.log(vote)
+
+//5)Logical operator
+//1)and operator
+// var income=true
+// var cibil=true
+// var creditcard=income&&cibil
+// console.log(creditcard)
+
+//2)or operator
+var income=false
+var cibil=false
+// var hdfccreditcard=income||cibil
+// console.log(hdfccreditcard)
+//3)not operator
+
+console.log(!income)
